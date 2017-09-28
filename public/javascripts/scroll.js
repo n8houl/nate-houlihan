8a9b03afa46d20235d90d9988b6f702d33a96554
n8houl <n8houl@gmail.com>
2017-09-28 17:40:13 -0400

    Header/Scroll Updates
    Fixed the header position and anchored it to one spot.  Added variable
    header size to allow for room for content viewing

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,9 +1,19 @@
 $('a[href*="#"]').click(function(event) {
   event.preventDefault();
   var target = $(this.hash);
+
   $('html, body').animate({
     scrollTop: target.offset().top
   }, 1000, function() {
+    if(window.scrollY > 50) {
+      $('#name').animate({
+        fontSize: '55px'
+      });
+    } else {
+      $('#name').animate({
+        fontSize: '110px'
+      });
+    }
     var $target = $(target);
     $target.focus();
     if($target.is(":focus")) {
@@ -13,4 +23,16 @@ $('a[href*="#"]').click(function(event) {
       $target.focus();
     };
   });
+});
+
+$(document).scroll(function () {
+  if(window.scrollY > 50) {
+    $('#name').stop().animate({
+      fontSize: '55px'
+    });
+  } else {
+    $('#name').stop().animate({
+      fontSize: '110px'
+    });
+  }
 });
