a1bd5c8636a83f4c463dc053dfe5668a849ee345
n8houl <n8houl@gmail.com>
2017-09-28 18:48:05 -0400

    Menu Fix / Card Flip Projects
    Added flipping card project view (WIP)
    Made header go behind content section (Menu Doesnt work above Work
    Section)

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -14,14 +14,14 @@ $('a[href*="#"]').click(function(event) {
         fontSize: '110px'
       });
     }
-    var $target = $(target);
-    $target.focus();
-    if($target.is(":focus")) {
-      return false;
-    } else {
-      $target.attr('tabindex', '-1');
-      $target.focus();
-    };
+//    var $target = $(target);
+//    $target.focus();
+//    if($target.is(":focus")) {
+//      return false;
+//    } else {
+//      $target.attr('tabindex', '-1');
+//      $target.focus();
+//    };
   });
 });
 
